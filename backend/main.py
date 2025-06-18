09581e73560c14e63144d4bb92dce79baf9de31e
Paramon Yevstigneyev <paramonplay+dev@gmail.com>
2025-06-18 22:19:49 +0300

    Implemented file uploading to the backend API

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
-
-from fastapi import FastAPI, UploadFile, status
+import os
+from fastapi import FastAPI, File, UploadFile, status
 from fastapi.requests import *
 from fastapi.responses import *
 
@@ -7,15 +7,21 @@ app = FastAPI()
 
 @app.get("/")
 async def load_frontend():
-    return Response("The frontend interface is currently in development, and will be redirected to there once it has basic functionality", status.HTTP_501_NOT_IMPLEMENTED)
+    return Response("The frontend will be in development soon...", status_code=status.HTTP_200_OK)
 
 @app.post("/upload")
 async def upload_log(file: UploadFile):
-    if not file.filename.endswith((".txt",".log")):
-        return Response("Invalid log file format", status_code=status.HTTP_406_NOT_ACCEPTABLE)
+
+    if not os.path.isdir("logs"):
+        os.mkdir("./logs")
+
+    if not file.filename.endswith((".txt",".log",".csv")):
+        return Response("Invalid log file format", status_code=status.HTTP_400_BAD_REQUEST)
 
     if file.size <= 0:
-        return Response("File is empty", status_code=status.HTTP_406_NOT_ACCEPTABLE)
+        return Response("File is empty", status_code=status.HTTP_400_BAD_REQUEST)
+
+    content = file.read()
 
     with open(f"logs/{file.filename}", "wb") as f:
         f.writelines(file.file.readlines())
@@ -24,7 +30,8 @@ async def upload_log(file: UploadFile):
 
 @app.get("/health")
 async def get_health():
-    return Response(status_code=status.HTTP_200_OK)
+    # Health check logic will be implemented later
+    return JSONResponse({"status", "healthy"}, status_code=status.HTTP_200_OK)
 
 @app.get("/version")
 async def get_version():
