2cf9e2c08f97c7860fc507f7bcfe3f65c0fb7a9e
Paramon Yevstigneyev <paramonplay+dev@gmail.com>
2025-07-16 06:35:19 +0300

    Cleaned up unused code and dependencies

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,8 @@
 import os
+import traceback
+
 from fastapi import FastAPI, UploadFile, status
+from fastapi.requests import *
 from fastapi.responses import *
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -15,11 +18,16 @@ from ML.SSH.ssh_model import analyze as analyze_ssh
 app = FastAPI()
 
 origins = [
-    "http://localhost:3000",
-    "http://localhost:8000"
+    "http://localhost:3000"
 ]
 
-app.add_middleware(CORSMiddleware, allow_origins=origins, allow_methods=["GET", "POST"], allow_headers=["*"])
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"]
+)
 
 @app.get("/")
 async def load_frontend():
@@ -27,7 +35,6 @@ async def load_frontend():
 
 @app.post("/upload")
 async def upload_log(file: UploadFile):
-
     if not os.path.isdir("logs"):
         os.mkdir("./logs")
     if not file.filename.endswith((".txt",".log",".csv")):
@@ -76,11 +83,3 @@ async def detect_threats(file: str, format: str):
 
         case _:
             return Response(f"\"{format}\" is not a supported log format", status_code=status.HTTP_400_BAD_REQUEST)
-
-@app.get("/results/{id}")
-async def get_result(result_id: int):
-    return Response("Result retrival is not yet implemented", status_code=status.HTTP_501_NOT_IMPLEMENTED)
-
-@app.get("/batch-detect")
-async def batch_detection():
-    return Response("Batch processing is not yet implemented", status_code=status.HTTP_501_NOT_IMPLEMENTED)
